3b9e349f2dec42a23216154fbc193e4321acaac9
Rio Saputro <rio.ae23@gmail.com>
2026-05-09 22:47:26 +0700

    fix: add CORS middleware to api-gateway for swagger UI

--- cmd/main.go
+++ cmd/main.go
@@ -38,6 +38,17 @@ func main() {
 
 	r := gin.Default()
 
+	r.Use(func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+			return
+		}
+		c.Next()
+	})
+
 	// Base Route
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
